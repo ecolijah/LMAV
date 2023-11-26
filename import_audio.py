f7cfdd4dcf93bcfd73415600f74aa11ffc031bc5
ecolijah <ecolijah@gmail.com>
2023-11-26 14:30:08 -0600

    bugs, but detecting bass peak logic is there i think

--- import_audio.py
+++ import_audio.py
@@ -5,7 +5,8 @@ import numpy as np
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
-CHUNK = 1024
+CHUNK = 1024 # number of audio frames to be examined in each chunk
+             # this is key to achieving low latency, to find a balance adjust this
 
 # CREATE PYAUDIO INSTANCE
 audio = pyaudio.PyAudio()
@@ -18,17 +19,38 @@ stream = audio.open(format=FORMAT, channels=CHANNELS,
 print("NOW CAPTURING AUDIO...")
 
 try:
-    while True:  # Keep processing audio chunks forever
+    while True:  # keep processing audio chunks forever
         data = stream.read(CHUNK)
-        # Convert the chunk to numpy array and calculate the volume
+        # convert the chunk to numpy array and calculate the volume
         numpydata = np.frombuffer(data, dtype=np.int16)
-        volume = np.linalg.norm(numpydata) * 10
-        print(f"Volume: {volume}")
+
+        # fast fourier transform, converts the time-domain audio signal into its frequency-domain representation.
+        fft_result = np.fft.fft(numpydata)
+        fft_abs = np.abs(fft_result)
+
+        #
+        freqs = np.fft.fftfreq(len(fft_result)) * RATE
+
+        bass_freq_range = (20, 160)  # Define bass frequency range
+        threshold = 1000000  # Define a threshold for detecting beats
+
+        for i, freq in enumerate(freqs):
+            if bass_freq_range[0] <= freq <= bass_freq_range[1]:
+                print(fft_abs[i])
+                if fft_abs[i] > threshold:
+                    print(f"Bass beat detected at frequency: {freq} Hz")
+                    # Add your video switching logic here
+
+
+
+        # for testing
+        # volume = np.linalg.norm(numpydata) * 10
+        # print(f"Volume: {volume}")
 
         # TODO: Add more sophisticated audio processing here
 
 except KeyboardInterrupt:
-    # Stop the stream gracefully
+    # Stop the stream gracefully, ctrl+c
     print("Finished processing.")
 
     stream.stop_stream()
